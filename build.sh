843ce31ad364c13ab42e3697e3de3434f8df39d3
rickaas <rtlindeman@gmail.com>
2013-04-21 13:25:16 +0200

    Attempt to fix build

--- build.sh
+++ build.sh
@@ -63,7 +63,9 @@ Prepare
 
 # build org.strategoxt.imp.debuggers.stratego.runtime
 cd $STR_RUNTIME_HOME
-ant -f build.main.xml -lib $ECJ_DIR
+ant -f build.main.xml -lib $ECJ_DIR "-Dlib.dir=$DIST_DIR_BASE/dist-libdsldi/release"
+
+exit
 
 # build stratego-di
 cd $STR_INSTRUMENT_HOME
